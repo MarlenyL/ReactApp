f6044b7aaeb61244665a072625761e2d71549d33
MarlenyL <you@example.com>
2019-11-04 22:20:04 -0600

    Updates

---
+++ precache-manifest.b39e48eac27e7e5a90a0cdade12d5deb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c9d116fc4bcd7d672b459ed69e72171",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "7ceec28caa4ee4120390",
+    "url": "/ReactApp/static/js/2.8a515f3c.chunk.js"
+  },
+  {
+    "revision": "3efc5ac6ea4451c40acb",
+    "url": "/ReactApp/static/js/main.00701f73.chunk.js"
+  },
+  {
+    "revision": "c2bf00dbb91a9863a493",
+    "url": "/ReactApp/static/js/runtime-main.01deff5a.js"
+  }
+]);
